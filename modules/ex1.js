f673c6d43688d24443679ecbf272ed8cbc9495bf
Addison Verger <addison.j.verger@gmail.com>
2019-02-04 21:45:37 -0600

    modules complete

--- modules/ex1.js
+++ modules/ex1.js
@@ -1,2 +1,3 @@
 // Write a statement that imports every value from "export.js"
 
+import { PI, add, bool } from './export.js';
--- modules/ex2.js
+++ modules/ex2.js
@@ -1,1 +1,9 @@
 // export two numbers and a function from this file. Check "import.js" to see how you should name these variables
+
+export let arg1 = 2;
+
+export let arg2 = 5;
+
+export function func() {
+    console.log('I am a function')
+}
